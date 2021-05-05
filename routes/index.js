01d913723af5a685f1d4fa5459fb1cb96bf230bc
Victor Orlunda <vicorlunda@gmail.com>
2021-05-05 23:33:33 +0100

    fix bug

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 const { getIndex } = require('../controllers/index');
 
 router.get('/', getIndex);

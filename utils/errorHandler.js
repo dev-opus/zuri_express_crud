dc97407f46ce41f116bb258363fb192ddf470fba
Victor Orlunda <vicorlunda@gmail.com>
2021-05-05 21:39:30 +0100

    implement method for handling server errors

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,2 +1,13 @@
 const catchAsyncErrors = fn => (req, res, next) =>
   fn(req, res, next).catch(next);
+
+const handleErrors = (err, res) => {
+  const { message } = err.message;
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    statusCode,
+    status: 'Server Error',
+    message,
+  });
+};

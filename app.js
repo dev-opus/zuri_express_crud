009695fec76a685e68c9f857732bb565e6faffe9
Victor Orlunda <vicorlunda@gmail.com>
2021-05-05 22:02:40 +0100

    implement boiler-plate middlewares in entry point

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const { handleErrors } = require('./utils/errorHandler');
+const port = process.env.PORT || 3000;
+
+// body parsing middleware section
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes section
+
+// handle unrecognised routes gracefully
+app.use('*', (req, res) => {
+  res.status(404).json({
+    statusCode: 404,
+    status: `404 not found. cannot ${req.method} ${req.originalUrl} on this API`,
+    availableRoutes: [{ index: '/', 'user-info': '/user-info' }],
+  });
+});
+
+// handle asynchronous/server errors propagated by catchAsyncErrors
+app.use((err, req, res, next) => {
+  handleErrors(err, res);
+});
+
+// fire the server!
+app.listen(port, () => console.log(`Server started on port ${port}`));
